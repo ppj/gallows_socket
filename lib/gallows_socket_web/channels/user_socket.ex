03dd9696592b0e8bcf484e7cc5dd9effe7bbd439
Prasanna Joshi <prasanna_3003@yahoo.com>
2019-07-26 18:27:51 +1000

    Configure channel support

--- lib/gallows_socket_web/channels/user_socket.ex
+++ lib/gallows_socket_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule GallowsSocketWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", GallowsSocketWeb.RoomChannel
+  channel "hangman:*", GallowsSocketWeb.HangmanChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
